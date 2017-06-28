be0b3a1c808b81ac388f9b0d0c5745f8d8c8774a
JM Labesse <jeanmarie.labesse@gmail.com>
2017-06-28 20:08:08 +0200

    Mise en place service Serialization

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,10 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class DefaultController extends Controller
 {
@@ -18,15 +14,10 @@ class DefaultController extends Controller
      */
     public function songAction(Request $request, $id)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Song")->find($id);
-        $jsonContent = $serializer->serialize($data, 'json');
-       return new JsonResponse(array('data' => $jsonContent));
+        $song = $em->getRepository("AppBundle:Song")->find($id);
+
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($song)));
     }
 
     /**
@@ -34,15 +25,10 @@ class DefaultController extends Controller
      */
     public function songsAction(Request $request)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Song")->findAll();
-        $jsonContent = $serializer->serialize($data, 'json');
-        return new JsonResponse(array('data' => $jsonContent));
+        $songs = $em->getRepository("AppBundle:Song")->findAll();
+
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($songs)));
     }
 
     /**
@@ -50,15 +36,10 @@ class DefaultController extends Controller
      */
     public function playlistAction(Request $request, $id)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Playlist")->find($id);
-        $jsonContent = $serializer->serialize($data, 'json');
-        return new JsonResponse(array('data' => $jsonContent));
+        $playlist = $em->getRepository("AppBundle:Playlist")->find($id);
+
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($playlist)));
     }
 
     /**
@@ -66,15 +47,10 @@ class DefaultController extends Controller
      */
     public function playlistsAction(Request $request)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Playlist")->findAll();
-        $jsonContent = $serializer->serialize($data, 'json');
-        return new JsonResponse(array('data' => $jsonContent));
+        $playlists = $em->getRepository("AppBundle:Playlist")->findAll();
+
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($playlists)));
     }
 
     /**
@@ -82,31 +58,20 @@ class DefaultController extends Controller
      */
     public function tagAction(Request $request, $id)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Tag")->find($id);
-        $jsonContent = $serializer->serialize($data, 'json');
-        return new JsonResponse(array('data' => $jsonContent));
+        $tag = $em->getRepository("AppBundle:Tag")->find($id);
+
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($tag)));
     }
 
     /**
      * @Route("/api/tags/", name="api_tags")
      */
-    public function tagsAction(Request $request, $id)
+    public function tagsAction(Request $request)
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
         $em = $this->getDoctrine()->getManager();
-        $data = $em->getRepository("AppBundle:Tag")->findAll();
-        $jsonContent = $serializer->serialize($data, 'json');
-        return new JsonResponse(array('data' => $jsonContent));
-    }
+        $tags = $em->getRepository("AppBundle:Tag")->findAll();
 
+        return new JsonResponse(array('data' => $this->get('app.serializer')->serializeObject($tags)));
+    }
 }
---
+++ src/AppBundle/Service/SerializeObject.php
@@ -0,0 +1,25 @@
+<?php
+namespace AppBundle\Service;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class SerializeObject
+{
+
+    public function serializeObject($object)
+    {
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $jsonContent = $serializer->serialize($object, 'json');
+
+        return $jsonContent;
+    }
+
+
+}
