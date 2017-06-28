d746151e550c03598ef5fc1ce7160476ec8ff0e2
bhaslag <bhaslag@gmail.com>
2017-06-29 02:56:24 +0800

    l

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -12,16 +12,17 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\BrowserKit\Request;
 
-class Post extends Controller
+class PostController extends Controller
 {
 
-    /**
-     * @Route("/api/post/", name="api_post")
-     */
-    public function postAction(Request $request)
-    {
-        $post = json_decode('request', $request);
-
-        $em = $this->getDoctrine()->getManager();
-    }
+//    /**
+//     * @Route("/api/post/", name="api_post")
+//     */
+//    public function postAction(Request $request)
+//    {
+//        $post = json_decode('request', $request);
+//
+//        $em = $this->getDoctrine()->getManager();
+//        return $this;
+//    }
 }
