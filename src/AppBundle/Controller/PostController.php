7d31b4771888b5ceb9c375cf6549ec3a368fb654
bhaslag <bhaslag@gmail.com>
2017-06-29 02:25:31 +0800

    controller post

---
+++ src/AppBundle/Controller/PostController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder
+ * Date: 28/06/17
+ * Time: 20:10
+ */
+
+namespace AppBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+
+class Post extends Controller
+{
+
+    /**
+     * @Route("/api/post/", name="api_post")
+     */
+    public function postAction(Request $request)
+    {
+        $post = json_decode('request', $request);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ()
+    }
+}
--- src/AppBundle/Service/API.php
+++ src/AppBundle/Service/API.php
@@ -5,32 +5,5 @@ use Symfony\Component\Config\Definition\Exception\Exception;
 
 class etsyCall
 {
-    private $apikey;
-    private $shopid;
 
-    public function __construct($apikey, $shopid)
-    {
-        $this->apikey = $apikey;
-        $this->shopid = $shopid;
-    }
-
-    public function etsyAction ()
-    {
-        $shop = "/shops/" . $this->shopid . "/listings/active?method=GET&api_key=";
-        $api = $this->apikey . "&fields=title,description,price,url&limit=100&includes=MainImage";
-
-        $url = "https://openapi.etsy.com/v2" . $shop . $api;
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec($ch);
-
-        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if (intval($status) != 200) throw new Exception("HTTP $status\n$result");
-
-        curl_close($ch);
-
-        $responseObject = json_decode($result);
-        return $responseObject;
-    }
 }
