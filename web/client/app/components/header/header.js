4580165e7f8d47513f4e21dbc21fde30a922d4d6
Bastien DURAN <bastien.narud@gmail.com>
2017-06-28 15:18:31 +0200

    installing jquey

--- web/client/app/components/header/header.js
+++ web/client/app/components/header/header.js
@@ -6,6 +6,13 @@ angular.module('DukeBox')
     templateUrl: './app/components/header/header.html',
     controller: function () {
       this.$onInit = () => {
+
+        // Initialize collapse button
+        $(".button-collapse").sideNav();
+        // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+        // $('.collapsible').collapsible();
+        
+
         this.states = [{
           name: 'home',
           displayName: 'Home'
