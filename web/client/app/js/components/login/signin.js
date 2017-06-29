b4b5ab44ab56384041b6fc949c3cdccad7b2d60e
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-06-29 11:26:50 +0200

    tentative connexion

--- web/client/app/js/components/login/signin.js
+++ web/client/app/js/components/login/signin.js
@@ -4,20 +4,35 @@ angular.module('DukeBox')
 
   .component('signin', {
     templateUrl: './app/js/components/login/signin.html',
-    controller: function (AuthService, $state, $window, CONSTANTS) {
+    controller: function (AuthService, UsersService, $state, $window, CONSTANTS) {
       'ngInject';
 
       this.$onInit = () => {}; //this.$onInit
 
       this.signin = () => {
-        console.log(this.user);
         // Local authentication mode
+        let new_user = {
+          'email': this.user.email,
+          'password': this.user.password
+        };
+        console.log(new_user);
+
         AuthService.login(this.user).then(() => {}).catch((err) => {
           // $mdToast.showSimple(`Error : ${err} !`);
           Materialize.toast(`Error : ${err} !`, 6000)
         });
       }; //this.signin
 
+      // this.signin = () => {
+      //   console.log(this.user);
+      //   // Local authentication mode
+      //   UsersService.save(this.user).then(() => {
+      //     console.log(this.user);
+      //   }).catch((err) => {
+      //     // $mdToast.showSimple(`Error : ${err} !`);
+      //     Materialize.toast(`Error : ${err} !`, 6000)
+      //   });
+      // }; //this.signin
 
     }
   });
