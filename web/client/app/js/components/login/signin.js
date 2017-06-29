1c51d4bbbc88a008b7afeb45691e144d69aaaa0e
Bastien DURAN <bastien.narud@gmail.com>
2017-06-29 09:26:10 +0200

    signin

--- web/client/app/js/components/login/signin.js
+++ web/client/app/js/components/login/signin.js
@@ -10,13 +10,14 @@ angular.module('DukeBox')
       this.$onInit = () => {}; //this.$onInit
 
       this.signin = () => {
-        // Local authentication mode
-        AuthService.login(this.user).then(() => {
-          $state.go('users');
-        }).catch((err) => {
-          // $mdToast.showSimple(`Error : ${err} !`);
-          Materialize.toast(`Error : ${err} !`, 6000)
-        });
+        console.log(this.user)
+        // // Local authentication mode
+        // AuthService.login(this.user).then(() => {
+        //   $state.go('users');
+        // }).catch((err) => {
+        //   // $mdToast.showSimple(`Error : ${err} !`);
+        //   Materialize.toast(`Error : ${err} !`, 6000)
+        // });
       }; //this.signin
 
 
